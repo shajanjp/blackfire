39109e883ec7431135d99d971880e4d511d06ecf
Shajan Jacob <shajanjp@gmail.com>
2017-03-29 19:40:26 +0530

    adds morgan and cookie parser

--- config/express.js
+++ config/express.js
@@ -1,13 +1,16 @@
 var config = require('./env/'+ process.env.NODE_ENV +'.js');
 var express = require('express');
+var morgan = require('morgan');
 var compress = require('compression');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var activeModules = require('./modules.js').activeModules;
 
 module.exports = function(){
 	var app = express();
-	
-	if (process.env.NODE_ENV == 'production') {
+	if(process.env.NODE_ENV === 'development'){
+		app.use(morgan('dev'));
+	}else if(process.env.NODE_ENV === 'production'){
 		app.use(compress());
 	}
 
