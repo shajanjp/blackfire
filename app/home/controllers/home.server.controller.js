fe114282b63ebe1027e55f43a65c62781e04a283
Shajan Jacob <shajanjp@gmail.com>
2017-10-27 17:06:41 +0530

    adds UI and API controllers

--- app/home/controllers/home.server.controller.js
+++ app/home/controllers/home.server.controller.js
@@ -1,3 +1,7 @@
-exports.home = function(req, res){
+exports.homeUI = function(req, res){
 	res.render('home/views/home');
+}
+
+exports.homeAPI = function(req, res){
+	res.json({ 'first_name' : 'Elon', 'last_name': 'Musk'});
 }
