1afd1c69d298def47db67b5c5900e415eb236610
Shajan Jacob <shajanjp@gmail.com>
2018-03-15 16:59:47 +0530

    adds module generator file

---
+++ machine.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const modulesDir = 'app' 
+const modulesListPath = 'config/modules_list.json';
+
+function makeFolder(folderPath) {
+	if (!fs.existsSync(folderPath)) {
+		fs.mkdirSync(folderPath);
+		console.log(`Folder ${folderPath} created.`);
+	}
+}
+
+function makeFile(filePath, content) {
+	fs.writeFile(filePath, content, (err) => {
+		if (err) throw err;
+		console.log(`File ${filePath} created.`);
+	});
+}
+
+function generateConfigFile(filePath, moduleName, moduleSingular) {
+	let moduleTitle = moduleName.charAt(0).toUpperCase() + moduleName.slice(1);
+	let configData = `{
+	"name" : "${moduleName.toLowerCase()}",
+	"title" : "${moduleTitle}",
+	"description" : "${moduleTitle} will be ${moduleName.toLowerCase()} !",
+	"routes": ["${moduleName.toLowerCase()}.server.route.js"],
+	"models": ["${moduleName.toLowerCase()}.server.model.js"],
+	"root": "/${moduleName.toLowerCase()}"
+}`;
+	makeFile(filePath, configData);
+}
+
+function generateControllerFile(filePath) {
+	let controllerData = `exports.home = function(req, res) {
+	return res.status(200).json({ "sucess": true });
+}`;
+	makeFile(filePath, controllerData);
+}
+
+function generateRouterFile(filePath, moduleName, moduleSingular) {
+	let routerData = `const express = require('express');
+const router = express.Router();
+const ${moduleSingular}Controller = require('../controllers/${moduleName}.server.controller.js');
+
+router.route('/')
+.get(${moduleSingular}Controller.home);
+
+module.exports = router;
+`;
+	makeFile(filePath, routerData);
+}
+
+function addModuleToList(moduleName, moduleAPIRoot) {
+	let moduleTitle = moduleName.charAt(0).toUpperCase() + moduleName.slice(1);
+	moduleName = moduleName.toLowerCase();
+
+	let moduleDataItem = {
+		"name": moduleName,
+		"title": moduleTitle,
+		"root": `/${moduleAPIRoot}`
+	};
+
+	fs.readFile(modulesListPath, 'utf8', (err, content) => {
+		console.log('original', content);
+		if (err) throw err;
+		let modulesList = JSON.parse(content);
+		modulesList.push(moduleDataItem);
+		console.log('modified', JSON.stringify(modulesList, null, 8));
+		makeFile(modulesListPath, JSON.stringify(modulesList, null, 2))
+	});
+}
+
+function makeModuleFilesAndFolders(moduleName, moduleSingular, moduleAPIRoot) {
+	let moduleRoot = `${modulesDir}/${moduleName}`;
+	makeFolder(`${moduleRoot}`);
+	makeFolder(`${moduleRoot}/config`);
+	makeFolder(`${moduleRoot}/controllers`);
+	makeFolder(`${moduleRoot}/libraries`);
+	makeFolder(`${moduleRoot}/models`);
+	makeFolder(`${moduleRoot}/routes`);
+
+	generateConfigFile(`${moduleRoot}/config/${moduleName}.config.json`, moduleName, moduleSingular); 
+	generateControllerFile(`${moduleRoot}/controllers/${moduleName}.server.controller.js`);
+	makeFile(`${moduleRoot}/libraries/${moduleName}.server.library.js`, ""); 
+	makeFile(`${moduleRoot}/models/${moduleName}.server.model.js`, ""); 
+	generateRouterFile(`${moduleRoot}/routes/${moduleName}.server.route.js`, moduleName, moduleSingular); 
+	addModuleToList(moduleName, moduleAPIRoot)
+}
+
+makeModuleFilesAndFolders("cars", "car", "cars");
