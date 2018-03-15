430c3139f43a23c1a52681ee510584de3ca0ccd1
Shajan Jacob <shajanjp@gmail.com>
2018-03-15 14:20:25 +0530

    removes home module files

--- app/home/controllers/home.server.controller.js
+++
@@ -1,3 +0,0 @@
-exports.home = function(req, res){
-	res.status(200).json({ "version": "1.0", title: "BlackFire" });
-}
--- app/home/routes/home.server.api.route.js
+++
@@ -1,7 +0,0 @@
-var homeController = require('../controllers/home.server.controller.js');
-var domainRoot = '/api';
-
-module.exports = function(app){
-	app.route(domainRoot + '/')
-	.get(homeController.home);
-}
