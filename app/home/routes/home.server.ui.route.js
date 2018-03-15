6ca402328bd5faaff8f60106a4b0760e64b4089e
Shajan Jacob <shajanjp@gmail.com>
2018-03-15 12:17:11 +0530

    removes home ui route

--- app/home/routes/home.server.ui.route.js
+++
@@ -1,7 +0,0 @@
-var homeController = require('../controllers/home.server.controller.js');
-var domainRoot = '';
-
-module.exports = function(app){
-	app.route(domainRoot + '/')
-	.get(homeController.homeUI);
-}
