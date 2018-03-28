8fc558e676e38c2e00010075febf8d1f2afca5e5
Shajan Jacob <shajanjp@gmail.com>
2018-03-28 09:35:10 +0530

    adds extra routes

--- machine.js
+++ machine.js
@@ -178,13 +178,14 @@ function generateRouterFile(filePath) {
 	let routerData = `const express = require('express');
 const router = express.Router();
 const ${moduleDetails.singular}Controller = require('../controllers/${moduleDetails.plural}.server.controller.js');
+const ${moduleDetails.singular}Validator = require('../libraries/${moduleDetails.plural}.server.validation.js');
 
 router.route('/')
-.post(${moduleDetails.singular}Controller.create${moduleDetails.singularCamel})
-.get(${moduleDetails.singular}Controller.list${moduleDetails.pluralCamel});
+.post(${moduleDetails.singular}Validator.validateInsert${moduleDetails.singularCamel}, ${moduleDetails.singular}Controller.insert${moduleDetails.singularCamel})
+.get(${moduleDetails.singular}Controller.get${moduleDetails.pluralCamel});
 
 router.route('/:${moduleDetails.singular}_id')
-.put(${moduleDetails.singular}Controller.update${moduleDetails.singularCamel})
+.put(${moduleDetails.singular}Validator.validateInsert${moduleDetails.singularCamel}, ${moduleDetails.singular}Controller.update${moduleDetails.singularCamel})
 .get(${moduleDetails.singular}Controller.get${moduleDetails.singularCamel})
 .delete(${moduleDetails.singular}Controller.remove${moduleDetails.singularCamel})
 
