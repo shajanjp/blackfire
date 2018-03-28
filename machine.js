bba95df161f2eedcb492cc7a253c6ac014629546
Shajan Jacob <shajanjp@gmail.com>
2018-03-28 09:35:30 +0530

    adds validation data

--- machine.js
+++ machine.js
@@ -195,6 +195,29 @@ module.exports = router;`;
 	makeFile(filePath, routerData);
 }
 
+function generateValidaionFile(filePath){
+	let validationFileData = `const joi = require('joi');
+const mongoId = joi.string().length(24);
+
+const ${moduleDetails.singular}InsertSchema = joi.object().keys({
+	title: joi.string().max(8).required()
+});
+
+exports.validateInsert${moduleDetails.singularCamel} = function (req, res, next) {
+	joi.validate(req.body, ${moduleDetails.singular}InsertSchema, { 'stripUnknown': true }, function (err, validated) {
+		if(err)
+			return res.status(500).json({ 
+				"errors": err.details[0].message
+			});
+		else {
+			res.locals.${moduleDetails.singular} = validated;
+			return next();
+		}
+	});
+}`;
+makeFile(filePath, validationFileData);
+}
+
 function addModuleToList() {
 	let moduleDataItem = {
 		"name": moduleDetails.plural,
