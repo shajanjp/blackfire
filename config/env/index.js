27b465bed8ceb6fcfee4a88c1e7bbe64031aa97f
Shajan Jacob <shajanjp@gmail.com>
2018-04-04 11:50:29 +0530

    fixes variable names and indentations

--- config/env/index.js
+++ config/env/index.js
@@ -1,9 +1,10 @@
-const ENV_USING = process.env.NODE_ENV || "default";
-if(typeof require(`./${ENV_USING}.json`) !== "object") {
-	console.log(`Couldnt find configuration file "${ENV_USING}.json" in env folder.`);
-	console.log(`Set NODE_ENV properly and restart.`);
-	process.exit();
+const ENV_USING = process.env.NODE_ENV || 'default';
+const configData = require(`./${ENV_USING}.json`);
+if (typeof configData !== 'object') {
+  console.log(`Couldnt find configuration file "${ENV_USING}.json" in env folder.`);
+  console.log('Set NODE_ENV properly and restart.');
+  process.exit();
 }
 console.log(`Using configuration from ${ENV_USING}.json`);
 
-module.exports = require(`./${ENV_USING}.json`);
+module.exports = configData;
