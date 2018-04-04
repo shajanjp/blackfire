80db508cf7938030a9fbe7b000603c689c36c1f9
Shajan Jacob <shajanjp@gmail.com>
2018-04-04 16:38:49 +0530

    fixes mongo schema loading order

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const config = require('./config/env');
 const mongoose = require('./config/mongoose');
 const express = require('./config/express');
 
-const app = express();
 mongoose();
+const app = express();
 
 app.listen(config.app.port, () => {
   console.log(`Server started at http://localhost:${config.app.port} using ${process.env.NODE_ENV || 'default'} config file`);
