ae2e2658d5e131d21b018c8d92072179971e841f
Shajan Jacob <shajanjp@gmail.com>
2018-04-06 12:18:59 +0530

    adds module model generator file

---
+++ utilities/generate-moduleModelFile.js
@@ -0,0 +1,13 @@
+const helperUtilities = require("../utilities/lib.generator.js");
+
+module.exports = function(filePath, moduleDetails) {
+  const modelFileData = `const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let ${moduleDetails.singular}Schema = new Schema({
+  title: String
+});
+
+mongoose.model('${moduleDetails.singular}', ${moduleDetails.singular}Schema);`;
+  helperUtilities.makeFile(filePath, modelFileData);
+}
