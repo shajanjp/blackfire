8164938b2e0e37d7656765decd93cdd026d391c1
Shajan Jacob <shajanjp@gmail.com>
2018-04-06 12:18:47 +0530

    adds module config generator file

---
+++ utilities/generator-moduleConfigFile.js
@@ -0,0 +1,13 @@
+const helperUtilities = require("./lib.generator.js");
+
+module.exports = function(filePath, moduleDetails) {
+  const configData = `{
+  "name" : "${moduleDetails.plural}",
+  "title" : "${moduleDetails.pluralCamel}",
+  "description" : "${moduleDetails.pluralCamel} will be ${moduleDetails.plural} !",
+  "routes": ["${moduleDetails.plural}.server.route.js"],
+  "models": ["${moduleDetails.plural}.server.model.js"],
+  "root": "/${moduleDetails.plural}"
+}`;
+  helperUtilities.makeFile(filePath, configData);
+}
