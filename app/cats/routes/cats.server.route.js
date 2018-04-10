50d36424b738bf6b2cd37c7c59769f1606fc53b6
Shajan Jacob <shajanjp@gmail.com>
2018-04-10 10:49:51 +0530

    removes cats module

--- app/cats/routes/cats.server.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router(); // eslint-disable-line new-cap
-const catController = require('../controllers/cats.server.controller.js');
-
-router.route('/')
-.get(catController.home);
-
-module.exports = router;
